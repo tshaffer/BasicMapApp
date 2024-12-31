16ef64ca957ed2264aab012cd0c97b8e20a24859
Ted Shaffer <shaffer.family@gmail.com>
2024-12-31 14:27:37 -0800

    remove reference to old leaked key

--- BasicMapApp/BasicMapAppApp.swift
+++ BasicMapApp/BasicMapAppApp.swift
@@ -4,10 +4,7 @@ import GoogleMaps
 @main
 struct BasicMapApp: App {
     init() {
-//        GMSServices.provideAPIKey("AIzaSyCaFpHHKdGvoyEJp3Z0NBiunu8Mymc25PQ")
-//        if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GoogleMapsAPIKey") as? String {
-//            GMSServices.provideAPIKey(apiKey)
-//        }
+
         if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GoogleMapsAPIKey") as? String {
             print("Retrieved API Key: \(apiKey)")
             GMSServices.provideAPIKey(apiKey)
