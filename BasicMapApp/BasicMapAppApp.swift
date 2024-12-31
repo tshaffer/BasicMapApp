75cc8568536b584d47341f547442e59cf1639a97
Ted Shaffer <shaffer.family@gmail.com>
2024-12-31 11:04:51 -0800

    It is again failing to find GoogleMaps

--- BasicMapApp/BasicMapAppApp.swift
+++ BasicMapApp/BasicMapAppApp.swift
@@ -4,7 +4,10 @@ import GoogleMaps
 @main
 struct BasicMapApp: App {
     init() {
-        GMSServices.provideAPIKey("")
+//        GMSServices.provideAPIKey("AIzaSyCaFpHHKdGvoyEJp3Z0NBiunu8Mymc25PQ")
+        if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GoogleMapsAPIKey") as? String {
+            GMSServices.provideAPIKey(apiKey)
+        }
     }
 
     var body: some Scene {
