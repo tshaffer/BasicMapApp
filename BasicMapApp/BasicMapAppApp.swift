7eafd819405307ddc868007ff2f94579401a9f02
Ted Shaffer <shaffer.family@gmail.com>
2024-12-31 12:08:57 -0800

    seems to be working....

--- BasicMapApp/BasicMapAppApp.swift
+++ BasicMapApp/BasicMapAppApp.swift
@@ -5,9 +5,16 @@ import GoogleMaps
 struct BasicMapApp: App {
     init() {
 //        GMSServices.provideAPIKey("AIzaSyCaFpHHKdGvoyEJp3Z0NBiunu8Mymc25PQ")
+//        if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GoogleMapsAPIKey") as? String {
+//            GMSServices.provideAPIKey(apiKey)
+//        }
         if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GoogleMapsAPIKey") as? String {
+            print("Retrieved API Key: \(apiKey)")
             GMSServices.provideAPIKey(apiKey)
+        } else {
+            print("Failed to retrieve the API key.")
         }
+
     }
 
     var body: some Scene {
