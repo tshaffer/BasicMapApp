c79daeaff124afdd9766424a896e77ca29957bf2
Ted Shaffer <shaffer.family@gmail.com>
2024-12-30 17:00:21 -0800

    Initial Commit

---
+++ BasicMapApp/BasicMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicMapAppApp.swift
+//  BasicMapApp
+//
+//  Created by Ted Shaffer on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
